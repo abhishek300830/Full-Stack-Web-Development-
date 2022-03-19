9b038fe7ed790f025d0547456be5b8f9a3dc6283
Abhishek Kumar <abhishek300830@gmail.com>
2022-03-20 00:39:29 +0530

    NODE Modified

--- Learnings/10 Node-js/Node_js_file_operation/writeFile.js
+++ Learnings/10 Node-js/Node_js_file_operation/writeFile.js
@@ -1,6 +1,6 @@
 const fs = require('fs') //inbuild library of node check details on node Documentation
 
-fs.writeFile(__dirname + '/createdFile.txt', "Some data", function (err) {
+fs.writeFile(__dirname + '/createdFile.txt', "Some NEW data", function (err) {
     if (err) throw err
     console.log('File was Written')
 })
